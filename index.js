4c7348103d7411de968fdac9a0bfd0bd80af0101
n810tran <nathaniel.k.tran@gmail.com>
2023-09-14 22:33:32 -0700

    added dependencies to repo, added packages and questions to questions array

--- index.js
+++ index.js
@@ -1,13 +1,61 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'Enter the project title:',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Enter a project description:',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Enter installation instructions:',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Enter usage information:',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Enter contribution guidelines:',
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'Enter test instructions:',
+        name: 'tests',
+    },
+    {
+        type: 'list',
+        message: 'Select a license for your project:',
+        choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'Other', 'None'],
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'Enter your GitHub username:',
+        name: 'githubUsername',
+    },
+    {
+        type: 'input',
+        message: 'Enter your email address:',
+        name: 'email',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
